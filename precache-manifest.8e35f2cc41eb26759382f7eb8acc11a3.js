6690ae753d2bd40041142b48dbbc7dfb82252a22
Masha-L <lifsh22m@mtholyoke.edu>
2020-04-21 13:01:13 -0400

    Updates

---
+++ precache-manifest.8e35f2cc41eb26759382f7eb8acc11a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f945d179a5efeb4b194df992e316fa",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "ccaf5b1018e02c179555",
+    "url": "/trivia/static/css/main.1d4555ef.chunk.css"
+  },
+  {
+    "revision": "2ccb7fd31565ed6306bd",
+    "url": "/trivia/static/js/2.b74b9514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia/static/js/2.b74b9514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaf5b1018e02c179555",
+    "url": "/trivia/static/js/main.aa8fbd71.chunk.js"
+  },
+  {
+    "revision": "b82f07857693b9a10991",
+    "url": "/trivia/static/js/runtime-main.9251dc87.js"
+  },
+  {
+    "revision": "8426da0a0ac2b79c51d3d282d1f73e01",
+    "url": "/trivia/static/media/1.8426da0a.png"
+  },
+  {
+    "revision": "3745fc01a4d94d45b9fdf4d80cf04143",
+    "url": "/trivia/static/media/2.3745fc01.png"
+  },
+  {
+    "revision": "c975f32d9ba70abe6c3eb480f88ba7b1",
+    "url": "/trivia/static/media/4.c975f32d.png"
+  }
+]);
